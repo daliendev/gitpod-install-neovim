19a4a651e9b75d8801a7810e1bfa3d68596e5bed
daniel cadeau <daniel.cadeau@proton.me>
2024-09-29 22:13:16 +0200

    feat(install script): path addapt due to Gitpod structure (+permissions)

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ apt-get update
 apt-get install -y build-essential curl git
 
 # Install Nerd Fonts (FiraCode as an example)
-mkdir -p ~/.local/share/fonts
-curl -fLo "~/.local/share/fonts/0xProto Nerd Font.ttf" \
+mkdir -p /home/gitpod/.local/share/fonts
+curl -fLo "/home/gitpod/.local/share/fonts/0xProto Nerd Font.ttf" \
     https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip
 fc-cache -fv
 
@@ -20,7 +20,7 @@ mkdir -p /home/gitpod/.local/bin
 ln -s $(pwd)/squashfs-root/AppRun /home/gitpod/.local/bin/nvim
 
 # Clone Neovim configuration 
-git clone --depth 1 https://github.com/daliendev/astro-nvim/ ~/.config/nvim
+git clone --depth 1 https://github.com/daliendev/astro-nvim/ /home/gitpod/.config/nvim
 
 # Install necessary LSP and plugins
 nvim --headless +Lazy! sync +qall
